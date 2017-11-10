32a9c99ed4fa76a025d8ecfca29527045ee72072
Ian <kivlov02@gmail.com>
2017-11-10 13:56:49 +0100

    bug fix

--- index.php
+++ index.php
@@ -23,18 +23,17 @@ if (! isset($_POST['tracking_number'])) {
 require './vendor/autoload.php';
 
 //track number = 40795800592
-$dotenv = new Dotenv\Dotenv(__DIR__);
-$dotenv->load();
+
 $soapClient = new SoapClient('shipments-tracking-api-wsdl.wsdl');
 
 	$params = array(
 		'ClientInfo' => array(
-									'AccountCountryCode'	=> getenv('AccountCountryCode'),
-									'AccountEntity'		 	=> getenv('AccountEntity'),
-									'AccountNumber'		 	=> getenv('AccountNumber'),
-									'AccountPin'		 	=> getenv('AccountPin'),
-									'UserName'			 	=> getenv('userid'),
-									'Password'			 	=> getenv('passcode'),
+									'AccountCountryCode'	=> 'ZA',
+									'AccountEntity'		 	=> 'DUR',
+									'AccountNumber'		 	=> '200721',
+									'AccountPin'		 	=> '543543',
+									'UserName'			 	=> 'shahil@planet54.com',
+									'Password'			 	=> '#E36m3bmw',
 									'Version'			 	=> 'v1.0'
 								),
 		'Transaction' => array('Reference1' => '001'),
@@ -44,14 +43,16 @@ $soapClient = new SoapClient('shipments-tracking-api-wsdl.wsdl');
 	// calling the method and printing results
 	try {
 		$auth_call = $soapClient->TrackShipments($params);
-		if($auth_call->Notifications->Notification->Code === 'ERR01') {
-			http_response_code(401);
-			print_r(json_encode([
-				"status"=> http_response_code(),
-				"message"=> "Unauthorized",
-				"payload" => $auth_call->Notifications->Notification->Message
-			]));
-			exit;
+		if(isset($auth_call->Notifications->Notification)) {
+			if($auth_call->Notifications->Notification->Code === 'ERR01') {
+				http_response_code(401);
+				print_r(json_encode([
+					"status"=> http_response_code(),
+					"message"=> "Unauthorized",
+					"payload" => $auth_call->Notifications->Notification->Message
+				]));
+				exit;
+			}
 		}
 
 		//json_encode($auth_call->TrackingResults)
